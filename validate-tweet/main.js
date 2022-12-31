9668e42208e38b966f1d887a9d626210a4ef1b7a
Martin More Panta <martinmore52@gmail.com>
2022-12-31 18:47:42 -0500

    initial commit

---
+++ validate-tweet/main.js
@@ -0,0 +1,12 @@
+module.exports = ({core, twitter}) => {
+    const tweetEncoded = process.env.TWEET;
+
+    const buff = Buffer.from(tweetEncoded, "base64");
+    const tweet = buff.toString("ascii");
+
+    const result = twitter.parseTweet(tweet);
+
+    if (!result.valid) {
+        core.setFailed(`Tweet is not valid!`);
+    }
+};
